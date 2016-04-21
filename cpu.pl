e4ccd4be6f04ae78a71668efdc328f041416efdd
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-21 17:08:14 +0400

    + std.program & OSes
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ cpu.pl
@@ -0,0 +1,8 @@
+% supported CPUs
+
+cpu(amd64,'Intel/AMD 80x86',bits(64)).
+cpu(xeon,'Intel Xeon',bits(64)).
+cpu(x86,'Intel i386',bits(32)).
+cpu(cortex,'ARM Cortex-M',bits(32)).
+cpu(avr8,'Atmel ATmega AVR',bits(8)).
+cpu(i8086,'Intel 8086',bits(16)).
--- pl.pl
+++ pl.pl
@@ -1,11 +1,31 @@
-% files
+% files (lex skeleton powered)
 
-file('Makefile').
+file('ypp.ypp').
+file('lpp.lpp').
 file('hpp.hpp').
 file('cpp.cpp').
+file('Makefile').
 
-files :-
-	file(X),
-	open(X,write,F),
-	close(F),
-	fail.
+consult('cpu.pl').
+
+% supported oses
+
+os(linux).
+banned_os(win32).
+banned_os(android).
+banned_os(ios).
+banned_os(raw).
+banned_os(dos).
+
+%% build/host/target triplets
+
+target(X) :- os(X).
+
+% std.programs
+
+cc("gcc").
+cxx("g++").
+
+make(build(linux),"make").
+make(build(win32),"mingw32-make").
+
