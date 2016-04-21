808daf7e3617a528a3aa85d349ba5efb8b94c833
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-21 17:38:28 +0400

    * make(build(OS),CMD)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- pl.pl
+++ pl.pl
@@ -4,10 +4,12 @@ file('ypp.ypp').
 file('lpp.lpp').
 file('hpp.hpp').
 file('cpp.cpp').
+file('Makefile').
 
 % CPUs
 
 cpu(x86,'Intel i386').
+cpu(xeon,'Intel Xeon x86_64').
 cpu(i8086,'Intel i8086').
 cpu(cm,'ARM Cortex-M').
 cpu(avr,'Atmel ATmega AVR').
@@ -20,6 +22,9 @@ os(linux).
 
 cc('gcc').
 cxx('g++').
-make('make').
+
+make(build(linux),'make').
+make(build(win32),'mingw32-make').
+
 lex('flex').
 yacc('bison').
