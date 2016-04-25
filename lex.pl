b2f1fc2df28f7fe0584cb0331180d2d898fcbbd1
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-25 15:19:31 +0400

    + lexer: file read to codes
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lex.pl
+++ lex.pl
@@ -1,5 +1,7 @@
 % lexical scaner
 
 lex :-
-		open('/home/ponyatov/my/skelex.bI',read,SRC),
-		write(SRC),nl.
+		open('/home/ponyatov/my/skelex.bI',read,SRCFILE),
+		read_stream_to_codes(SRCFILE,SRC),
+		close(SRCFILE),
+		write(SRC).
