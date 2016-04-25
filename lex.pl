25f122d67a88111478873d5fb3dc1d4f30d15011
Dmitry Ponyatov <dponyatov@gmail.com>
2016-04-25 17:05:23 +0400

    + lgintro
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lex.pl
+++ lex.pl
@@ -1,7 +1,47 @@
 % lexical scaner
 
-lex :-
-		open('/home/ponyatov/my/skelex.bI',read,SRCFILE),
-		read_stream_to_codes(SRCFILE,SRC),
-		close(SRCFILE),
-		write(SRC).
+%:- use_module(library(pio)).
+
+%eol --> [ '\r\n' ]. % win32/dos
+%eol --> [ '\n'   ]. % unix
+%
+%tbl -->	[ '\t'   ].	% tabulation
+%spc --> [ ' '    ].	% space
+%
+%space --> [] ; eol ; tbl ; spc .	% any blank char (space)
+%
+%%letter(X) --> [X],{char_type(alpha)}.
+%%dig(X) --> [X],{char_type(digit)}.
+%
+%lex --> [].
+%lex --> space.
+%%lex --> ( space ; letter ; dig ) , lex.
+
+%lex(X) --> {X=10}.
+%lex([]) --> []. 
+%lex(X) --> [X], {X='\n'}.
+%lex([_|T]) --> [lex(T).
+
+%		%,
+%		atom_chars(S,SRC),format("~s~s",[S]).
+%		%phrase(lex,SRC,OUT),format("",[OUT]).
+
+space(X) --> [X],{code_type(X, space)}.
+spaces([]) --> [].
+spaces([H|T]) --> space(H),!,spaces(T).
+	
+char(X)  --> [X],{\+ code_type(X, space)}.
+chars([]) --> [].
+chars([H|T]) --> char(H),!,chars(T).
+
+lex([H|T]) --> (spaces(H);char(H)),lex(T).
+lex([]) --> [].
+
+go :- phrase_from_file(lex(X),'/home/ponyatov/my/skelex.bI'),write(X),nl.
+%go :-
+%		open('/home/ponyatov/my/skelex.bI',read,SRCFILE),
+%		read_stream_to_codes(SRCFILE,SRC),
+%		close(SRCFILE),
+%		phrase(lex(X),SRC),write(X),nl.
+
+:- initialization(go;true).
---
+++ lgintro.pl
@@ -0,0 +1,5 @@
+% http://cmsmcq.com/2004/lgintro.html
+%% to definite clause grammars and definite clause translation grammars
+%% A working paper prepared for the W3C XML Schema Working Group
+%% (c) C. M. Sperberg-McQueen
+
