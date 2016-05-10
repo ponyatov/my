66cf193f8580b86a0b8e55f51c41eea3f13fe62d
Dmitry Ponyatov <dponyatov@gmail.com>
2016-05-10 15:54:51 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- bratko/lists.pl
+++ bratko/lists.pl
@@ -6,3 +6,7 @@ ht([H|T]) :- write(H),nl,write(T).
 %List1 = [a ,b,c ] ,
 %List2 = .(a, .( b, .( c, []))).
 
+in([],_) :- write('A'),!.
+in(H,[H|_]) :- write('B'),!.
+in(X,[_|L]) :- write('C'),in(X,L).
+
